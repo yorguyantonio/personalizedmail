66f2e5b6e34b02ee27c1e2a35548355f5bbe33bc
Yorguy Antonio Batista Desdin <yorguyantoniobd@gmail.com>
2018-11-02 17:41:50 -0400

    Including nodemailer as main dependency.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-    console.log("testing ssh ");
+'use strict';
 
-};
+var lib = require('./lib/mailsender.js');
+module.exports = lib();
--- test.js
+++
@@ -1,4 +0,0 @@
-var index=require("./index");
-
-
-index.printMsg();
